2f826e2022c483e5df55d5eb70189a308061c7b3
Perimili sunil kumar <perimilisunil@gmail.com>
2026-07-20 00:52:29 +0530

    Update Script.js

--- business_rules/01_Lock_Project_Field/Script.js
+++ business_rules/01_Lock_Project_Field/Script.js
@@ -1,9 +1,15 @@
-(function executeRule(current, previous /*null when async*/) {
+(function executeRule(current, previous) {
 
-    // Only relevant on update, when Project field actually changed
-    if (current.project.changes() && !gs.hasRole('role_manager')) {
-        current.setAbortAction(true);
-        gs.addErrorMessage('Only a Project Manager can change the Project on an existing task.');
+    if (previous.state != 3)
+        return;
+
+    var project = current.project.getRefRecord();
+
+    if (project.getValue('project_manager') == gs.getUserID()) {
+        return;
     }
+
+    gs.addErrorMessage("This task is already completed. Only the Project Manager can modify completed tasks.");
+    current.setAbortAction(true);
 
 })(current, previous);
